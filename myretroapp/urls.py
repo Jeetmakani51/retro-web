04c15722fa8cb1a6f2101bd7ec873ab5ca06e406
Jeet <makanijeet51@gmail.com>
2026-07-07 21:32:25 +0530

    added authentication

--- myretroapp/urls.py
+++ myretroapp/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.homepage),
+    path('', views.register_view, name = 'register_view'),
+    path('register', views.register_view, name = 'register_view'),
+    path('login', views.login_view, name = 'login_view'),
+    path('home', views.home_view, name = 'home_view'),
+    path('logout', views.logout_view, name = 'logout_view')
 ]
--- myretroapp/views.py
+++ myretroapp/views.py
@@ -1,6 +1,52 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
-def homepage(request):
-    return HttpResponse("this is homepage")
+def register_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        email = request.POST['email']
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "Username already exists!")
+            return redirect('register_view')
+        
+        User.objects.create_user(
+            username=username,
+            password=password,
+            email=email
+        )
+        messages.success(request, "registration successful")
+        return redirect('login_view')
+    return render(request, 'index.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(
+            username=username,
+            password=password
+        )
+
+        if user is not None:
+            login(request, user)
+            return redirect('home_view')
+        else:
+            messages.error(request, "Invalid username or password")
+    return render(request, 'login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect(login_view)
+
+@login_required(login_url="login")
+def home_view(request):
+    return render(request, 'home.html')
